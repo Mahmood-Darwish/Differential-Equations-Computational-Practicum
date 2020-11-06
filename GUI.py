713d28c39deef1cf8ba6a91a8de8fcf27591b93d
Mahmood-Darwish <m.darwish@innopolis.university>
2020-11-06 16:48:38 +0300

    put classes

--- GUI.py
+++ GUI.py
@@ -1,158 +1,162 @@
 from tkinter import *
 from tkinter import ttk
 from matplotlib import pyplot
-import Solver
+from Solver import solver
 
-def draw_all_graphs():
-    try:
-        numberofsteps = int(box_steps.get())
-        x0 = float(box_x.get())
-        y0 = float(box_y.get())
-        X = float(box_end.get())
-    except:
-        numberofsteps = 10
-        x0 = 0
-        y0 = 1
-        X = 7
-    if numberofsteps > 1000000 or numberofsteps < 10:
-        numberofsteps = 10
-    if X <= x0:
-        X = x0 + 7
-    res = Solver.EulerMethod(x0, y0, numberofsteps, X)
-    pyplot.plot(res[0], res[1], label="Euler")
-    res = Solver.ImprovedEulerMethod(x0, y0, numberofsteps, X)
-    pyplot.plot(res[0], res[1], label="Improv. Euler")
-    res = Solver.RungeKuttaMethod(x0, y0, numberofsteps, X)
-    pyplot.plot(res[0], res[1], label="Runge-Kutta")
-    res = Solver.solve(x0, y0, numberofsteps, X)
-    pyplot.plot(res[0], res[1], label="Exact")
-    pyplot.legend()
-    pyplot.show()
-
-
-def draw_all_error_graphs():
-    try:
-        numberofsteps = int(box_steps.get())
-        x0 = float(box_x.get())
-        y0 = float(box_y.get())
-        X = float(box_end.get())
-    except:
-        numberofsteps = 10
-        x0 = 0
-        y0 = 1
-        X = 7
-    if numberofsteps > 1000000 or numberofsteps < 10:
-        numberofsteps = 10
-    if X <= x0:
-        X = x0 + 7
-    res = Solver.EulerMethod_error(x0, y0, numberofsteps, X)
-    pyplot.plot(res[0], res[1], label="Euler")
-    res = Solver.ImprovedEulerMethod_error(x0, y0, numberofsteps, X)
-    pyplot.plot(res[0], res[1], label="Improv. Euler")
-    res = Solver.RungeKuttaMethod_error(x0, y0, numberofsteps, X)
-    pyplot.plot(res[0], res[1], label="Runge-Kutta")
-    pyplot.legend()
-    pyplot.show()
+class GUI:
+    def draw_all_graphs(self):
+        try:
+            numberofsteps = int(self.box_steps.get())
+            x0 = float(self.box_x.get())
+            y0 = float(self.box_y.get())
+            X = float(self.box_end.get())
+        except:
+            numberofsteps = 10
+            x0 = 0
+            y0 = 1
+            X = 7
+        if numberofsteps > 1000000 or numberofsteps < 10:
+            numberofsteps = 10
+        if X <= x0:
+            X = x0 + 7
+        res = solver.EulerMethod(x0, y0, numberofsteps, X)
+        pyplot.plot(res[0], res[1], label="Euler")
+        res = solver.ImprovedEulerMethod(x0, y0, numberofsteps, X)
+        pyplot.plot(res[0], res[1], label="Improv. Euler")
+        res = solver.RungeKuttaMethod(x0, y0, numberofsteps, X)
+        pyplot.plot(res[0], res[1], label="Runge-Kutta")
+        res = solver.solve(x0, y0, numberofsteps, X)
+        pyplot.plot(res[0], res[1], label="Exact")
+        pyplot.legend()
+        pyplot.show()
 
 
-def analyse():
-    try:
-        n0 = int(box_n0.get())
-        n1 = int(box_n1.get())
-    except:
-        n0 = 10
-        n1 = 100
-    if n0 < 1:
-        n0 = 10
-    if n1 < n0:
-        n1 = n0 + 100
-    res_Euler = [[], []]
-    for i in range(n0, n1 + 1):
-        res_Euler[0].append(i)
-        res_Euler[1].append(Solver.EulerMethod_maxerror(i))
-    res_ImprovedEuler = [[], []]
-    for i in range(n0, n1 + 1):
-        res_ImprovedEuler[0].append(i)
-        res_ImprovedEuler[1].append(Solver.ImprovedEulerMethod_maxerror(i))
-    res_RungeKutta = [[], []]
-    for i in range(n0, n1 + 1):
-        res_RungeKutta[0].append(i)
-        res_RungeKutta[1].append(Solver.RungeKuttaMethod_maxerror(i))
-    pyplot.plot(res_Euler[0], res_Euler[1], label="Euler")
-    pyplot.plot(res_ImprovedEuler[0], res_ImprovedEuler[1], label="Improved Euler")
-    pyplot.plot(res_RungeKutta[0], res_RungeKutta[1], label="Runge-Kutta")
-    pyplot.legend()
-    pyplot.show()
+    def draw_all_error_graphs(self):
+        try:
+            numberofsteps = int(self.box_steps.get())
+            x0 = float(self.box_x.get())
+            y0 = float(self.box_y.get())
+            X = float(self.box_end.get())
+        except:
+            numberofsteps = 10
+            x0 = 0
+            y0 = 1
+            X = 7
+        if numberofsteps > 1000000 or numberofsteps < 10:
+            numberofsteps = 10
+        if X <= x0:
+            X = x0 + 7
+        res = solver.EulerMethod_error(x0, y0, numberofsteps, X)
+        pyplot.plot(res[0], res[1], label="Euler")
+        res = solver.ImprovedEulerMethod_error(x0, y0, numberofsteps, X)
+        pyplot.plot(res[0], res[1], label="Improv. Euler")
+        res = solver.RungeKuttaMethod_error(x0, y0, numberofsteps, X)
+        pyplot.plot(res[0], res[1], label="Runge-Kutta")
+        pyplot.legend()
+        pyplot.show()
 
 
-root = Tk()
-root.title("Equation Solver")
+    def analyse(self):
+        try:
+            n0 = int(self.box_n0.get())
+            n1 = int(self.box_n1.get())
+        except:
+            n0 = 10
+            n1 = 100
+        if n0 < 1:
+            n0 = 10
+        if n1 < n0:
+            n1 = n0 + 100
+        res_Euler = [[], []]
+        for i in range(n0, n1 + 1):
+            res_Euler[0].append(i)
+            res_Euler[1].append(solver.EulerMethod_maxerror(i))
+        res_ImprovedEuler = [[], []]
+        for i in range(n0, n1 + 1):
+            res_ImprovedEuler[0].append(i)
+            res_ImprovedEuler[1].append(solver.ImprovedEulerMethod_maxerror(i))
+        res_RungeKutta = [[], []]
+        for i in range(n0, n1 + 1):
+            res_RungeKutta[0].append(i)
+            res_RungeKutta[1].append(solver.RungeKuttaMethod_maxerror(i))
+        pyplot.plot(res_Euler[0], res_Euler[1], label="Euler")
+        pyplot.plot(res_ImprovedEuler[0], res_ImprovedEuler[1], label="Improved Euler")
+        pyplot.plot(res_RungeKutta[0], res_RungeKutta[1], label="Runge-Kutta")
+        pyplot.legend()
+        pyplot.show()
 
-root.minsize(400, 400)
-root.maxsize(400, 400)
+    def start(self):
+        self.root = Tk()
+        self.root.title("Equation Solver")
 
-notebook = ttk.Notebook(root)
-notebook.pack()
-Frame1 = Frame(notebook, height=500, width=500)
-Frame2 = Frame(notebook, height=500, width=500)
-Frame1.pack()
-Frame2.pack()
-notebook.add(Frame1, text="Graphing")
-notebook.add(Frame2, text="Error Analysis")
+        self.root.minsize(400, 400)
+        self.root.maxsize(400, 400)
 
-label_title = Label(Frame1, text="f(x, y) = sec(x) - y tg(x)")
-label_title.place(x=200, y=10, anchor="center")
+        self.notebook = ttk.Notebook(self.root)
+        self.notebook.pack()
+        self.Frame1 = Frame(self.notebook, height=500, width=500)
+        self.Frame2 = Frame(self.notebook, height=500, width=500)
+        self.Frame1.pack()
+        self.Frame2.pack()
+        self.notebook.add(self.Frame1, text="Graphing")
+        self.notebook.add(self.Frame2, text="Error Analysis")
 
-box_steps = Entry(Frame1)
-box_steps.place(x=100, y=20)
-box_steps.insert(END, '10')
+        self.label_title = Label(self.Frame1, text="f(x, y) = sec(x) - y tg(x)")
+        self.label_title.place(x=200, y=10, anchor="center")
 
-label_steps = Label(Frame1, text="number of steps:")
-label_steps.place(x=5, y=20)
+        self.box_steps = Entry(self.Frame1)
+        self.box_steps.place(x=100, y=20)
+        self.box_steps.insert(END, '10')
 
-box_x = Entry(Frame1)
-box_x.place(x=100, y=40)
-box_x.insert(END, '0')
+        self.label_steps = Label(self.Frame1, text="number of steps:")
+        self.label_steps.place(x=5, y=20)
 
-label_x = Label(Frame1, text="x0:")
-label_x.place(x=5, y=40)
+        self.box_x = Entry(self.Frame1)
+        self.box_x.place(x=100, y=40)
+        self.box_x.insert(END, '0')
 
-box_y = Entry(Frame1)
-box_y.place(x=100, y=60)
-box_y.insert(END, '1')
+        self.label_x = Label(self.Frame1, text="x0:")
+        self.label_x.place(x=5, y=40)
 
-label_y = Label(Frame1, text="y0:")
-label_y.place(x=5, y=60)
+        self.box_y = Entry(self.Frame1)
+        self.box_y.place(x=100, y=60)
+        self.box_y.insert(END, '1')
 
-box_end = Entry(Frame1)
-box_end.place(x=100, y=80)
-box_end.insert(END, '7')
+        self.label_y = Label(self.Frame1, text="y0:")
+        self.label_y.place(x=5, y=60)
 
-label_end = Label(Frame1, text="X:")
-label_end.place(x=5, y=80)
+        self.box_end = Entry(self.Frame1)
+        self.box_end.place(x=100, y=80)
+        self.box_end.insert(END, '7')
 
-draw_all_button = Button(Frame1, text="draw all graphs", command=lambda: draw_all_graphs())
-draw_all_button.place(anchor="center", x=200, y=200)
+        self.label_end = Label(self.Frame1, text="X:")
+        self.label_end.place(x=5, y=80)
 
-draw_all_error_button = Button(Frame1, text="draw all error graphs", command=lambda: draw_all_error_graphs())
-draw_all_error_button.place(anchor="center", x=200, y=225)
+        self.draw_all_button = Button(self.Frame1, text="draw all graphs", command=lambda: self.draw_all_graphs())
+        self.draw_all_button.place(anchor="center", x=200, y=200)
 
-box_n0 = Entry(Frame2)
-box_n0.place(x=50, y=20)
-box_n0.insert(END, '10')
+        self.draw_all_error_button = Button(self.Frame1, text="draw all error graphs", command=lambda: self.draw_all_error_graphs())
+        self.draw_all_error_button.place(anchor="center", x=200, y=225)
 
-label_n0 = Label(Frame2, text="n_start:")
-label_n0.place(x=5, y=20)
+        self.box_n0 = Entry(self.Frame2)
+        self.box_n0.place(x=50, y=20)
+        self.box_n0.insert(END, '10')
 
-box_n1 = Entry(Frame2)
-box_n1.place(x=53, y=42)
-box_n1.insert(END, '100')
+        self.label_n0 = Label(self.Frame2, text="n_start:")
+        self.label_n0.place(x=5, y=20)
 
-label_n1 = Label(Frame2, text="n_finish:")
-label_n1.place(x=5, y=42)
+        self.box_n1 = Entry(self.Frame2)
+        self.box_n1.place(x=53, y=42)
+        self.box_n1.insert(END, '100')
 
-analyse_button = Button(Frame2, text="Analyse", command=lambda: analyse())
-analyse_button.place(anchor="center", x=200, y=200)
+        self.label_n1 = Label(self.Frame2, text="n_finish:")
+        self.label_n1.place(x=5, y=42)
+
+        self.analyse_button = Button(self.Frame2, text="Analyse", command=lambda: self.analyse())
+        self.analyse_button.place(anchor="center", x=200, y=200)
 
 
-root.mainloop()
+        self.root.mainloop()
+
+a = GUI()
+a.start()
--- Solver.py
+++ Solver.py
@@ -8,119 +8,123 @@ def F(x, C):
 def f(x, y):
     return (1 / math.cos(x)) - (y * math.tan(x))
 
-
-def solve(x0, y0, numofsteps, X):
-    C = (y0/math.cos(x0)) - math.tan(x0)
-    res = [[], []]
-    x = x0
-    h = (X - x0) / numofsteps
-    for i in range(numofsteps + 1):
-        res[0].append(x)
-        res[1].append(F(x, C))
-        x += h
-    return res
-
-
-def EulerMethod(x0, y0, numofsteps, X):
-    res = [[], []]
-    y = y0
-    x = x0
-    h = (X - x0) / numofsteps
-    for i in range(numofsteps + 1):
-        res[0].append(x)
-        res[1].append(y)
-        y += f(x, y) * h
-        x += h
-    return res
+class solver():
+    @staticmethod
+    def solve(x0, y0, numofsteps, X):
+        C = (y0/math.cos(x0)) - math.tan(x0)
+        res = [[], []]
+        x = x0
+        h = (X - x0) / numofsteps
+        for i in range(numofsteps + 1):
+            res[0].append(x)
+            res[1].append(F(x, C))
+            x += h
+        return res
 
 
-def ImprovedEulerMethod(x0, y0, numofsteps, X):
-    res = [[], []]
-    y = y0
-    x = x0
-    h = (X - x0) / numofsteps
-    for i in range(numofsteps + 1):
-        res[0].append(x)
-        res[1].append(y)
-        y += (f(x, y) + f(x + h, y + (h * f(x, y)))) * (h / 2)
-        x += h
-    return res
+    @staticmethod
+    def EulerMethod(x0, y0, numofsteps, X):
+        res = [[], []]
+        y = y0
+        x = x0
+        h = (X - x0) / numofsteps
+        for i in range(numofsteps + 1):
+            res[0].append(x)
+            res[1].append(y)
+            y += f(x, y) * h
+            x += h
+        return res
 
 
-def RungeKuttaMethod(x0, y0, numofsteps, X):
-    def T(x, y, h):
-        k1 = f(x, y)
-        k2 = f(x + (h / 2), y + (0.5 * h * k1))
-        k3 = f(x + (h / 2), y + (0.5 * h * k2))
-        k4 = f(x + h, y + (h * k3))
-        return (k1 + (2 * k2) + (2 * k3) + k4) / 6
-
-    res = [[], []]
-    y = y0
-    x = x0
-    h = (X - x0) / numofsteps
-    for i in range(numofsteps + 1):
-        res[0].append(x)
-        res[1].append(y)
-        y += T(x, y, h) * h
-        x += h
-    return res
+    @staticmethod
+    def ImprovedEulerMethod(x0, y0, numofsteps, X):
+        res = [[], []]
+        y = y0
+        x = x0
+        h = (X - x0) / numofsteps
+        for i in range(numofsteps + 1):
+            res[0].append(x)
+            res[1].append(y)
+            y += (f(x, y) + f(x + h, y + (h * f(x, y)))) * (h / 2)
+            x += h
+        return res
 
 
-def EulerMethod_error(x0, y0, numberofsteps, N):
-    res1 = EulerMethod(x0, y0, numberofsteps, N)
-    res2 = solve(x0, y0, numberofsteps, N)
-    res = [[], []]
-    for i in range(len(res1[0])):
-        res[0].append(res1[0][i])
-        res[1].append(abs(res1[1][i] - res2[1][i]))
-    return res
+    @staticmethod
+    def RungeKuttaMethod(x0, y0, numofsteps, X):
+        def T(x, y, h):
+            k1 = f(x, y)
+            k2 = f(x + (h / 2), y + (0.5 * h * k1))
+            k3 = f(x + (h / 2), y + (0.5 * h * k2))
+            k4 = f(x + h, y + (h * k3))
+            return (k1 + (2 * k2) + (2 * k3) + k4) / 6
+
+        res = [[], []]
+        y = y0
+        x = x0
+        h = (X - x0) / numofsteps
+        for i in range(numofsteps + 1):
+            res[0].append(x)
+            res[1].append(y)
+            y += T(x, y, h) * h
+            x += h
+        return res
+
+    @staticmethod
+    def EulerMethod_error(x0, y0, numberofsteps, N):
+        res1 = solver.EulerMethod(x0, y0, numberofsteps, N)
+        res2 = solver.solve(x0, y0, numberofsteps, N)
+        res = [[], []]
+        for i in range(len(res1[0])):
+            res[0].append(res1[0][i])
+            res[1].append(abs(res1[1][i] - res2[1][i]))
+        return res
+
+    @staticmethod
+    def ImprovedEulerMethod_error(x0, y0, numberofsteps, N):
+        res1 = solver.ImprovedEulerMethod(x0, y0, numberofsteps, N)
+        res2 = solver.solve(x0, y0, numberofsteps, N)
+        res = [[], []]
+        for i in range(len(res1[0])):
+            res[0].append(res1[0][i])
+            res[1].append(abs(res1[1][i] - res2[1][i]))
+        return res
+
+    @staticmethod
+    def RungeKuttaMethod_error(x0, y0, numberofsteps, N):
+        res1 = solver.RungeKuttaMethod(x0, y0, numberofsteps, N)
+        res2 = solver.solve(x0, y0, numberofsteps, N)
+        res = [[], []]
+        for i in range(len(res1[0])):
+            res[0].append(res1[0][i])
+            res[1].append(abs(res1[1][i] - res2[1][i]))
+        return res
 
 
-
-def ImprovedEulerMethod_error(x0, y0, numberofsteps, N):
-    res1 = ImprovedEulerMethod(x0, y0, numberofsteps, N)
-    res2 = solve(x0, y0, numberofsteps, N)
-    res = [[], []]
-    for i in range(len(res1[0])):
-        res[0].append(res1[0][i])
-        res[1].append(abs(res1[1][i] - res2[1][i]))
-    return res
-
+    @staticmethod
+    def EulerMethod_maxerror(n):
+        res1 = solver.solve(0, 1, n, 1.5)
+        res2 = solver.EulerMethod(0, 1, n, 1.5)
+        ans = -1
+        for i in range(len(res1[0])):
+            ans = max(ans, abs(res1[1][i] - res2[1][i]))
+        return ans
 
 
-def RungeKuttaMethod_error(x0, y0, numberofsteps, N):
-    res1 = RungeKuttaMethod(x0, y0, numberofsteps, N)
-    res2 = solve(x0, y0, numberofsteps, N)
-    res = [[], []]
-    for i in range(len(res1[0])):
-        res[0].append(res1[0][i])
-        res[1].append(abs(res1[1][i] - res2[1][i]))
-    return res
+    @staticmethod
+    def ImprovedEulerMethod_maxerror(n):
+        res1 = solver.solve(0, 1, n, 1.5)
+        res2 = solver.ImprovedEulerMethod(0, 1, n, 1.5)
+        ans = -1
+        for i in range(len(res1[0])):
+            ans = max(ans, abs(res1[1][i] - res2[1][i]))
+        return ans
 
-
-def EulerMethod_maxerror(n):
-    res1 = solve(0, 1, n, 1.5)
-    res2 = EulerMethod(0, 1, n, 1.5)
-    ans = -1
-    for i in range(len(res1[0])):
-        ans = max(ans, abs(res1[1][i] - res2[1][i]))
-    return ans
-
-
-def ImprovedEulerMethod_maxerror(n):
-    res1 = solve(0, 1, n, 1.5)
-    res2 = ImprovedEulerMethod(0, 1, n, 1.5)
-    ans = -1
-    for i in range(len(res1[0])):
-        ans = max(ans, abs(res1[1][i] - res2[1][i]))
-    return ans
-
-
-def RungeKuttaMethod_maxerror(n):
-    res1 = solve(0, 1, n, 1.5)
-    res2 = RungeKuttaMethod(0, 1, n, 1.5)
-    ans = -1
-    for i in range(len(res1[0])):
-        ans = max(ans, abs(res1[1][i] - res2[1][i]))
-    return ans
+    @staticmethod
+    def RungeKuttaMethod_maxerror(n):
+        res1 = solver.solve(0, 1, n, 1.5)
+        res2 = solver.RungeKuttaMethod(0, 1, n, 1.5)
+        ans = -1
+        for i in range(len(res1[0])):
+            ans = max(ans, abs(res1[1][i] - res2[1][i]))
+        return ans
